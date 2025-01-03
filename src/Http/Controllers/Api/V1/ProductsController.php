646ab49acd372a134a9c9a75260ab8bc14ad0017
xxl4 <nice.lizhi@gmail.com>
2025-01-03 08:48:33 +0000

    add http odoo api

---
+++ src/Http/Controllers/Api/V1/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+namespace NexaMerchant\OdooApi\Http\Controllers\Api\V1;
+
+use Illuminate\Support\Facades\Log;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+use NexaMerchant\OdooApi\Http\Controllers\Api\Controller;
+
+class ProductsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json();
+    }
+
+    public function show(Request $request, $id)
+    {
+        return response()->json();
+    }
+}
+
